fb3e87f435609a03816028b1e31fd28bea02994d
Michal Starski <michalstarski99@gmail.com>
2019-01-20 00:51:03 +0100

    table-generator 1.0

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const http = require("http");
-const { resolve } = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 const server = http.createServer(app);
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.get("/", function(req, res) {
 	res.render("form");
 });
 
-app.post("/api/generate-document", function(req, res) {
-	console.log("Working!");
+app.post("/api/generate-table", function(req, res) {
+	res.render("table", { table_data: req.body });
 });
 
 server.listen(8111, console.log("Listening on 8111"));
